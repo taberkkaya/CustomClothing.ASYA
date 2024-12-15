5adb910eb76de130d24704d0f192cbbfc58bcd94
taberkkaya <taberkkaya@gmail.com>
2024-12-15 05:37:59 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CustomClothing.ASYA.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
